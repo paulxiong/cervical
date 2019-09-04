2825aeae2d3a9eb700972cf282d07259b21c4b17
Lambda zhang <ggxxde@163.com>
2019-09-04 16:48:00 +0800

    添加一个简单的脚本来执行一连串的测试

---
+++ autokeras/2_run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+tasklist='task.list'
+
+if [ ! -f ${tasklist} ] || [ ! -f main.py ]; then
+	echo "not found main.py or "${tasklist}
+fi
+
+cnt=0
+while read line
+do
+  line2=$(echo ${line} | sed 's/ //g')
+  # 跳过空白行， 跳过'#'好开头的行
+  if [ ${#line2} -lt 1 ] || [ ${line2:0:1}'x' == '#x' ]; then
+      continue
+  fi
+  cnt=$(( $cnt + 1 ))
+
+  array=(${line2//,/ })
+  if [ ! ${#array[@]} -eq 4 ]; then
+      echo "invalied task!!!  "${line2}
+  fi
+
+  task_type=$(echo ${array[0]} | sed 's/"//g')
+  task_dir=$(echo ${array[1]} | sed 's/"//g')
+  task_mod=$(echo ${array[2]} | sed 's/"//g')
+  task_desc=$(echo ${array[3]} | sed 's/"//g')
+
+  echo $cnt'  '${task_desc}
+  echo 'python main.py --task '${task_type}' --taskdir '${task_dir}' --modfile '${task_mod}
+  python main.py --task ${task_type} --taskdir ${task_dir} --modfile ${task_mod} | tee $cnt'.log'
+  echo ""
+done < ${tasklist}
