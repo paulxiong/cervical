719408693e2628f0c388c060a2410a8c6d4c1c06
inpm <inpm.cy@gmail.com>
2019-09-11 14:26:08 +0800

    修改git提交+配置环境变量+打包config+swagger

--- webpage/4_vue-element-admin/src/const/config.js
+++ webpage/4_vue-element-admin/src/const/config.js
@@ -1,12 +1,12 @@
 // for release
-// export const APIUrl = "http://medical.raidcdn.cn:3000"
-// export const ImgServerUrl = "http://medical.raidcdn.cn:3001"
-
-// for debug
-
-// export const APIUrl = "http://9200.gpu.raidcdn.cn:9700"
-export const APIUrl = "http://api.gpu.raidcdn.cn:9700"
-export const ImgServerUrl = "http://9201.gpu.raidcdn.cn:9700"
+export const APIUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'http://medical.raidcdn.cn:3000'
+    : 'http://9200.gpu.raidcdn.cn:9700'
+export const ImgServerUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'http://medical.raidcdn.cn:3001'
+    : 'http://9201.gpu.raidcdn.cn:9700'
 
 // for local debug
 // export const APIUrl = "http://192.168.1.100:9000"
