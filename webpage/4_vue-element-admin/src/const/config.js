396e8a4c4f56c1b3a3448c17d9054bd0134cee77
lambda-zhang <ggxxde@163.com>
2019-09-18 13:51:33 +0800

    本地测试服务器直接访问域名

--- webpage/4_vue-element-admin/src/const/config.js
+++ webpage/4_vue-element-admin/src/const/config.js
@@ -2,11 +2,11 @@
 export const APIUrl =
   process.env.NODE_ENV === 'production'
     ? 'http://medical.raidcdn.cn:3000'
-    : 'http://9200.gpu.raidcdn.cn:9700'
+    : 'http://dev.medical.raidcdn.cn:3000'
 export const ImgServerUrl =
   process.env.NODE_ENV === 'production'
     ? 'http://medical.raidcdn.cn:3001'
-    : 'http://9201.gpu.raidcdn.cn:9700'
+    : 'http://dev.medical.raidcdn.cn:3001'
 
 // for local debug
 // export const APIUrl = 'http://192.168.1.100:9000'
