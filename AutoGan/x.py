2c820f5a2961cdd4b481b0a1024ce4c7c7d45133
houjianjiang <747147811@qq.com>
2019-09-26 02:05:08 +0000

    更新AutoGan分支代码

---
+++ AutoGan/x.py
@@ -0,0 +1,22 @@
+import PIL.Image as Image
+
+file1 = 'my_data_batch'
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='iso-8859-1')
+    return dict
+
+dict_train_batch1 = unpickle(file1)
+data_train_batch1 = dict_train_batch1.get('data')
+labels = dict_train_batch1.get('labels')
+img1 = dict_train_batch1['data'][10]
+a = img1.reshape(3, 32, 32)
+
+# 得到RGB通道
+r = Image.fromarray(a[0]).convert('L')
+g = Image.fromarray(a[1]).convert('L')
+b = Image.fromarray(a[2]).convert('L')
+image = Image.merge("RGB", (r, g, b))
+# 显示图片
+image.save("lambda.png", 'png')
