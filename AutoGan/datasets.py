2531837ac5d85739eaa202cceb3aea4033a2eb60
houjianjiang <747147811@qq.com>
2019-10-09 08:42:00 +0000

    更新脚本的超惨,修改训练样本的类别

--- AutoGan/datasets.py
+++ AutoGan/datasets.py
@@ -7,7 +7,7 @@ from torch.utils.data import Dataset
 class ImageDataset(object):
     def __init__(self, args):
         if args.dataset.lower() == 'cifar10':
-            Dt = datasets.CIFAR10          
+            Dt = datasets.CIFAR10
             transform = transforms.Compose([
                 transforms.Resize(args.img_size),
                 transforms.ToTensor(),
@@ -39,12 +39,11 @@ class ImageDataset(object):
         else:
             print("data_path:",args.data_path)
             Dt2 = Dt(root=args.data_path, train=True, transform=transform, download=True)
-            Dt1 = Dt(root=args.data_path, train=False, transform=transform)          
+            Dt1 = Dt(root=args.data_path, train=False, transform=transform)
             print("Dt_type:",type(Dt2))
-            print("Dt:",Dt2)         
+            print("Dt:",Dt2)
             print("Dt1_type:",type(Dt1))
-            print("Dt1:",Dt1)
-                     
+            print("Dt1:",Dt1)      
             self.train = torch.utils.data.DataLoader(
                 Dt2,
                 batch_size=args.dis_batch_size, shuffle=True,
@@ -57,7 +56,7 @@ class ImageDataset(object):
                 num_workers=args.num_workers, pin_memory=True)
 
             self.test = self.valid
-            
+
 class ImageDataset1(object):
     def __init__(self, args):
         if args.dataset.lower() == 'cifar10':
@@ -96,14 +95,13 @@ class ImageDataset1(object):
             print("data_path:",args.data_path)
             Dt2 = Dt(root=args.data_path, train=True, transform=transform, download=True)
             Dt1 = Dt(root=args.data_path, train=False, transform=transform)
-            
+ 
             print("Dt_type:",type(Dt2))
             print("Dt:",Dt2)
-            
+
             print("Dt1_type:",type(Dt1))
             print("Dt1:",Dt1)
-            
-            
+
             self.train = torch.utils.data.DataLoader(
                 Dt2,
                 batch_size=args.dis_batch_size, shuffle=True,
@@ -116,9 +114,4 @@ class ImageDataset1(object):
                 num_workers=args.num_workers, pin_memory=True)
 
             self.test = self.valid
-            
-            
-            
-            
-            
             
