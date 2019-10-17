132aaa904e701d57509883cf66ce97eeb1c7b739
yfq512 <1085561337@qq.com>
2019-10-17 07:52:32 +0000

    增加输出recall/precision/F1

--- autokeras/main.py
+++ autokeras/main.py
@@ -18,6 +18,7 @@ from autokeras.image.image_supervised import load_image_dataset, ImageClassifier
 from keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 from shutil import copyfile, rmtree
+import keras.backend as K
 
 def timestamp():
     return time.strftime("%Y%m%d_%H%M%S", time.localtime())
@@ -45,7 +46,7 @@ def resize_img(input_dir, output_dir, RESIZE):
             img = cv2.imread("%s/%s/%s"%(input_dir,cls_name,img_name))
             if img.shape[0] != img.shape[1]:
                 print("skip this image w != h: %s" % img_name)
-                continue
+                #continue
             img = cv2.resize(img,(RESIZE,RESIZE),interpolation=cv2.INTER_LINEAR)
             if os.path.exists("%s/%s"%(output_dir,cls_name)):
                 cv2.imwrite("%s/%s/%s"%(output_dir,cls_name,img_name),img)
@@ -69,6 +70,7 @@ class cervical_autokeras():
         self.PREDICT_IMG_DIR = os.path.join(self.ROOTPATH, 'predict')
         self.RESIZE_PREDICT_IMG_DIR = os.path.join(self.ROOTPATH, 'resize_predict')
         self.PREDICT_ERROR_IMG_DIR = os.path.join(self.ROOTPATH, 'predict_error_data')
+        self.PREDICT_RIGHT_IMG_DIR = os.path.join(self.ROOTPATH, 'predict_right_data')
         #Path to generate csv file
         self.TRAIN_CSV_DIR = os.path.join(self.ROOTPATH, 'train_labels.csv')
         self.TEST_CSV_DIR = os.path.join(self.ROOTPATH, 'test_labels.csv')
@@ -83,7 +85,7 @@ class cervical_autokeras():
         self.clean_fold()
 
     def clean_fold(self):
-        dirs = [self.RESIZE_TRAIN_IMG_DIR, self.RESIZE_TEST_IMG_DIR, self.RESIZE_PREDICT_IMG_DIR, self.PREDICT_ERROR_IMG_DIR]
+        dirs = [self.RESIZE_TRAIN_IMG_DIR, self.RESIZE_TEST_IMG_DIR, self.RESIZE_PREDICT_IMG_DIR, self.PREDICT_ERROR_IMG_DIR, self.PREDICT_RIGHT_IMG_DIR]
         files = [self.TRAIN_CSV_DIR, self.TEST_CSV_DIR, self.PREDICT_CSV_DIR]
         for d in dirs:
             if os.path.exists(d):
@@ -95,12 +97,19 @@ class cervical_autokeras():
 
     def train_autokeras(self):
         #Load images
-        train_data, train_labels = load_image_dataset(csv_file_path=self.TRAIN_CSV_DIR, images_path=self.RESIZE_TRAIN_IMG_DIR)
+        train_data_, train_labels_ = load_image_dataset(csv_file_path=self.TRAIN_CSV_DIR, images_path=self.RESIZE_TRAIN_IMG_DIR)
         test_data, test_labels = load_image_dataset(csv_file_path=self.TEST_CSV_DIR, images_path=self.RESIZE_TEST_IMG_DIR)
 
-        train_data = train_data.astype('float32') / 255
+        train_data_ = train_data_.astype('float32') / 255
         test_data = test_data.astype('float32') / 255
-        print("Train data shape:", train_data.shape)
+        print("Train data shape:", train_data_.shape)
+        # shuff train data and label
+        np.random.seed(10)
+        shuffle_indices = np.random.permutation(np.arange(len(train_labels_)))
+        train_data = train_data_[shuffle_indices]
+        train_labels = train_labels_[shuffle_indices]
+        train_data = train_data_
+        train_labels = train_labels_
 
         clf = ImageClassifier(verbose=True, path=self.TEMP_DIR)
         clf.fit(train_data, train_labels, time_limit=self.TIME)
@@ -121,16 +130,37 @@ class cervical_autokeras():
 
         autokeras_model = pickle_from_file(self.MODEL_DIR)
         autokeras_score = autokeras_model.evaluate(test_data, test_labels)
-        print(autokeras_score)
+        print('acu', autokeras_score)
+    
+    def print_score(self, label_record, total_num, false_num):
+        count = 0
+        for label in label_record:
+            TP = total_num[count]-false_num[count]
+            FN = false_num[count]
+            FP = np.sum(false_num) - false_num[count]
+            TN = (np.sum(total_num) - np.sum(false_num)) - (total_num[count] - false_num[count])
+            recall = TP/(TP + FP)
+            precision = TP/(TP + FN)
+            F1 = 2*(recall * precision)/(recall + precision)
+            accuracy = (TP + TN)/(TP + TN + FP + FN)
+            print('类型:%s - recall:%.4f - precision:%.4f - F1:%.4f - accuracy:%.4f '% (label, recall, precision, F1, accuracy))
+            count = count + 1
 
     def predict_autokeras2(self):
         autokeras_model = pickle_from_file(self.MODEL_DIR)
 
+        count1_temp = 0
+        label_record = []
+        len_ = len(os.listdir(self.RESIZE_PREDICT_IMG_DIR))
+        total_num = np.zeros(len_)
+        false_num = np.zeros(len_)
         #Load images
         for label in os.listdir(self.RESIZE_PREDICT_IMG_DIR):
             images = os.listdir(os.path.join(self.RESIZE_PREDICT_IMG_DIR, label))
             total = len(images)
             count_false = 0
+            count_true = 0
+            label_pre_ = []
             for index in range(0, total):
                 img_path = os.path.join(self.RESIZE_PREDICT_IMG_DIR, label, images[index])
                 if not os.path.exists(img_path):
@@ -141,6 +171,7 @@ class cervical_autokeras():
                 x = x.astype('float32') / 255
                 x = np.reshape(x, (1, self.RESIZE, self.RESIZE, 3))
                 y = autokeras_model.predict(x)
+                label_pre_.append(y[0])
                 if str(label) != str(y[0]):
                     #print("%s %s result=%s" % (images[index], label, y[0]))
                     count_false = count_false + 1
@@ -148,7 +179,19 @@ class cervical_autokeras():
                     if not os.path.exists(error_image_dir):
                         os.makedirs(error_image_dir)
                     copyfile(img_path, os.path.join(error_image_dir, images[index]))
+                else:
+                    count_true = count_true + 1
+                    right_image_dir = os.path.join(self.PREDICT_RIGHT_IMG_DIR, label)
+                    if not os.path.exists(right_image_dir):
+                        os.makedirs(right_image_dir)
+                    copyfile(img_path, os.path.join(right_image_dir, images[index]))
+                    
             print("%s 的个数/准确率：%d %f 出错的个数%d" % (label, total, (total - count_false) / total, count_false))
+            label_record.append(label)
+            total_num[count1_temp] = total
+            false_num[count1_temp] = count_false
+            count1_temp = count1_temp + 1
+        self.print_score(label_record, total_num, false_num)
 
 if __name__ == "__main__":
     ca = cervical_autokeras(opt.taskdir)
