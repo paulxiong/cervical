1de8553411c5b15f5180be42a786a4bd8de493d9
qixia717138552 <717138552@qq.com>
2019-10-18 17:43:41 +0800

    时间展示

--- webpage/4_vue-element-admin/src/utils/index.js
+++ webpage/4_vue-element-admin/src/utils/index.js
@@ -8,6 +8,7 @@
  * @param {string} cFormat
  * @returns {string}
  */
+
 export function parseTime(time, cFormat) {
   if (arguments.length === 0) {
     return null
@@ -50,6 +51,47 @@ export function parseTime(time, cFormat) {
  * @param {number} time
  * @param {string} option
  * @returns {string}
+ * created_at: 2019-10-18T06:38:34Z
+ * //JavaScript函数：
+ * //字符串转换为时间戳
+    function getDateTimeStamp (dateStr) {
+      return Date.parse(dateStr.replace(/-/gi,"/"));
+    }
+      var minute = 1000 * 60;
+      var hour = minute * 60;
+      var day = hour * 24;
+      var halfamonth = day * 15;
+      var month = day * 30;
+      function getDateDiff(dateTimeStamp){
+      var now = new Date().getTime();
+      var diffValue = now - dateTimeStamp;
+      if(diffValue < 0){
+      //若日期不符则弹出窗口告之
+      //alert("结束日期不能小于开始日期！");
+      }
+      var monthC =diffValue/month;
+      var weekC =diffValue/(7*day);
+      var dayC =diffValue/day;
+      var hourC =diffValue/hour;
+      var minC =diffValue/minute;
+      if(monthC>=1){
+      result="发表于" + parseInt(monthC) + "个月前";
+      }
+      else if(weekC>=1){
+      result="发表于" + parseInt(weekC) + "周前";
+      }
+      else if(dayC>=1){
+      result="发表于"+ parseInt(dayC) +"天前";
+      }
+      else if(hourC>=1){
+      result="发表于"+ parseInt(hourC) +"个小时前";
+      }
+      else if(minC>=1){
+      result="发表于"+ parseInt(minC) +"分钟前";
+      }else
+      result="刚刚发表";
+      return result;
+      }
  */
 export function formatTime(time, option) {
   if (('' + time).length === 10) {
