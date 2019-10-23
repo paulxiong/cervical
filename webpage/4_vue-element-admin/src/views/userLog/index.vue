86a4fbe556874408448637231018c03067c35277
inpm <inpm.cy@gmail.com>
2019-10-23 09:46:30 +0800

    用户日志table样式修改

--- webpage/4_vue-element-admin/src/views/userLog/index.vue
+++ webpage/4_vue-element-admin/src/views/userLog/index.vue
@@ -27,26 +27,26 @@
       <el-table-column fixed="right" label="操作" width="100">
         <template slot-scope="scope">
           <el-popover placement="right" width="400" trigger="click">
-            <table id="tfhover" class="tftable" border="1">
-              <tr><th>ID:</th><th>{{ scope.row.id }}</th></tr>
-              <tr><td>用户ID:</td><td>{{ scope.row.user_id }}</td></tr>
-              <tr><td>请求大小:</td><td>{{ scope.row.bodysize }}</td></tr>
-              <tr><td>状态码:</td><td>{{ scope.row.code }}</td></tr>
-              <tr><td>耗时(ms):</td><td>{{ scope.row.cost }}</td></tr>
-              <tr><td>创建时间:</td><td>{{ scope.row.created_at }}</td></tr>
-              <tr><td>请求方式:</td><td>{{ scope.row.method }}</td></tr>
-              <tr><td>路径:</td><td>{{ scope.row.path }}</td></tr>
-              <tr><td>参数:</td><td>{{ scope.row.query }}</td></tr>
-              <tr><td>来源:</td><td>{{ scope.row.referer }}</td></tr>
-              <tr><td>地域ID:</td><td>{{ scope.row.region_id }}</td></tr>
-              <tr><td>国家:</td><td>{{ scope.row.region.country }}</td></tr>
-              <tr><td>省份:</td><td>{{ scope.row.region.province }}</td></tr>
-              <tr><td>城市:</td><td>{{ scope.row.region.city }}</td></tr>
-              <tr><td>运营商:</td><td>{{ scope.row.region.isp }}</td></tr>
-              <tr><td>硬件:</td><td>{{ scope.row.ua.device.type }}</td></tr>
-              <tr><td>操作系统:</td><td>{{ scope.row.ua.os.name }}-{{ scope.row.ua.os.version.original }}</td></tr>
-              <tr><td>浏览器:</td><td>{{ scope.row.ua.browser.name }}-{{ scope.row.ua.browser.version.original }}</td></tr>
-              <tr><td>浏览器内核:</td><td>{{ scope.row.ua.engine.name }}{{ scope.row.ua.engine.version.original }}-</td></tr>
+            <table class="tftable" border="1">
+              <tr><th class="td-1">ID:</th><th>{{ scope.row.id }}</th></tr>
+              <tr><td class="td-1">用户ID:</td><td>{{ scope.row.user_id }}</td></tr>
+              <tr><td class="td-1">请求大小:</td><td>{{ scope.row.bodysize }}</td></tr>
+              <tr><td class="td-1">状态码:</td><td>{{ scope.row.code }}</td></tr>
+              <tr><td class="td-1">耗时(ms):</td><td>{{ scope.row.cost }}</td></tr>
+              <tr><td class="td-1">创建时间:</td><td>{{ scope.row.created_at }}</td></tr>
+              <tr><td class="td-1">请求方式:</td><td>{{ scope.row.method }}</td></tr>
+              <tr><td class="td-1">路径:</td><td>{{ scope.row.path }}</td></tr>
+              <tr><td class="td-1">参数:</td><td>{{ scope.row.query }}</td></tr>
+              <tr><td class="td-1">来源:</td><td>{{ scope.row.referer }}</td></tr>
+              <tr><td class="td-1">地域ID:</td><td>{{ scope.row.region_id }}</td></tr>
+              <tr><td class="td-1">国家:</td><td>{{ scope.row.region.country }}</td></tr>
+              <tr><td class="td-1">省份:</td><td>{{ scope.row.region.province }}</td></tr>
+              <tr><td class="td-1">城市:</td><td>{{ scope.row.region.city }}</td></tr>
+              <tr><td class="td-1">运营商:</td><td>{{ scope.row.region.isp }}</td></tr>
+              <tr><td class="td-1">硬件:</td><td>{{ scope.row.ua.device.type }}</td></tr>
+              <tr><td class="td-1">操作系统:</td><td>{{ scope.row.ua.os.name }}</td></tr>
+              <tr><td class="td-1">浏览器:</td><td>{{ scope.row.ua.browser.name }}</td></tr>
+              <tr><td class="td-1">浏览器内核:</td><td>{{ scope.row.ua.engine.name }}</td></tr>
             </table>
             <el-button slot="reference" type="text" size="small" @click="handleClick(scope.row)">查看</el-button>
           </el-popover>
@@ -140,5 +140,8 @@ table.tftable td {
   padding: 8px;
   border-style: solid;
   border-color: #729ea5;
+}
+.tftable .td-1 {
+  width: 100px;
 }
 </style>
