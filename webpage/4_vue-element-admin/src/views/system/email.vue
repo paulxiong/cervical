30bef7c818061ab6467f3fd48c604da313d1d823
qixia717138552 <717138552@qq.com>
2019-11-04 13:55:22 +0800

    邮件页面

--- webpage/4_vue-element-admin/src/views/system/email.vue
+++ webpage/4_vue-element-admin/src/views/system/email.vue
@@ -1,53 +1,45 @@
 <template>
   <div class="email">
-    <el-form ref="ruleForm" :model="ruleForm" :rules="rules" label-width="100px" class="demo-ruleForm">
-      <el-form-item label="用户名称" prop="name">
-        <el-input v-model="ruleForm.name" />
-      </el-form-item>
-      <el-form-item label="用户类型" prop="region">
-        <el-select v-model="ruleForm.region" placeholder="请选择用户类型">
-          <el-option label="管理员" value="shanghai" />
-          <el-option label="普通用户" value="beijing" />
-        </el-select>
-      </el-form-item>
-      <el-form-item label="修改时间" required>
-        <el-col :span="11">
-          <el-form-item prop="date1">
-            <el-date-picker v-model="ruleForm.date1" type="date" placeholder="选择日期" style="width: 100%;" />
-          </el-form-item>
-        </el-col>
-        <el-col class="line" :span="2">-</el-col>
-        <el-col :span="11">
-          <el-form-item prop="date2">
-            <el-time-picker v-model="ruleForm.date2" placeholder="选择时间" style="width: 100%;" />
-          </el-form-item>
-        </el-col>
-      </el-form-item>
-      <el-form-item label="确认信息" prop="delivery">
-        <el-switch v-model="ruleForm.delivery" />
-      </el-form-item>
-      <el-form-item label="邮件通知" prop="type">
-        <el-checkbox-group v-model="ruleForm.type">
-          <el-checkbox label="QQ邮箱" name="type" />
-          <el-checkbox label="Google邮箱" name="type" />
-          <el-checkbox label="网易邮箱" name="type" />
-          <el-checkbox label="雅虎邮箱" name="type" />
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="邮件订阅" prop="resource">
-        <el-radio-group v-model="ruleForm.resource">
-          <el-radio label="是" />
-          <el-radio label="否" />
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="添加备注" prop="desc">
-        <el-input v-model="ruleForm.desc" type="textarea" />
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
-        <el-button @click="resetForm('ruleForm')">重置</el-button>
-      </el-form-item>
-    </el-form>
+    <div class="header">
+      <el-row>
+        <el-button type="text" class="writing1">注册验证码</el-button>
+        <el-button type="info" icon="el-icon-message" circle />
+      </el-row>
+    </div>
+    <div class="middle">
+      <el-table
+        :data="emailList"
+        style="width: 100%"
+      />
+      <p class="middle1">讯动医疗 &lt;system@service.xundong.org&gt; </p>
+    </div>
+    <div class="middle2">
+      <el-dropdown split-button type="primary" @click="handleClick">
+        发送至
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item disabled>自己</el-dropdown-item>
+          <el-dropdown-item>管理员</el-dropdown-item>
+          <el-dropdown-item>客服</el-dropdown-item>
+          <el-dropdown-item>其他人员</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
+    <div class="middle3">
+      <p class="writing2">
+        讯动医疗
+      </p>
+      <el-badge is-dot class="item">账号注册验证码</el-badge>
+      <el-badge is-dot class="item">
+        <el-button class="share-button" icon="el-icon-share" type="primary" />
+      </el-badge>
+    </div>
+    <div class="footer">
+      <span>您好，您正在使用<p class="writing3">****xundong@gmail.com</p>注册账号。</span>
+      <span>您的注册验证码为：<p class="writing4">000000</p></span>
+    </div>
+    <div class="end">
+      <span>谢谢！</span>
+    </div>
   </div>
 </template>
 
@@ -57,55 +49,17 @@ export default {
   components: {},
   data() {
     return {
-      ruleForm: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
-      },
-      rules: {
-        name: [
-          { required: true, message: '请输入活动名称', trigger: 'blur' },
-          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
-        ],
-        region: [
-          { required: true, message: '请选择活动区域', trigger: 'change' }
-        ],
-        date1: [
-          { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
-        ],
-        date2: [
-          { type: 'date', required: true, message: '请选择时间', trigger: 'change' }
-        ],
-        type: [
-          { type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
-        ],
-        resource: [
-          { required: true, message: '请选择活动资源', trigger: 'change' }
-        ],
-        desc: [
-          { required: true, message: '请填写活动形式', trigger: 'blur' }
-        ]
-      }
+      emailList: [
+        {
+          'name': '讯动医疗',
+          'id': 'xundong@163.com'
+        }
+      ]
     }
   },
   methods: {
-    submitForm(formName) {
-      this.$refs[formName].validate((valid) => {
-        if (valid) {
-          alert('submit!')
-        } else {
-          console.log('error submit!!')
-          return false
-        }
-      })
-    },
-    resetForm(formName) {
-      this.$refs[formName].resetFields()
+    handleClick() {
+      alert('button click')
     }
   }
 }
@@ -113,5 +67,51 @@ export default {
 
 <style lang="scss" scoped>
 .email {
+  margin-top: 40px;
+  margin-left: 50px;
+}
+.writing1 {
+  color: black;
+  font-size: 80px;
+  font-weight: bold;
+}
+.middle1 {
+  font-weight: bold;
+  font-size: 20px;
+}
+.writing2 {
+  font-size: 40px;
+}
+.el-dropdown {
+  vertical-align: top;
+}
+.el-dropdown + .el-dropdown {
+  margin-left: 15px;
+}
+.el-icon-arrow-down {
+  font-size: 12px;
+}
+.footer {
+  margin-top: 50px;
+}
+.item {
+  margin-top: 10px;
+  margin-right: 40px;
+}
+.item:first-of-type {
+  font-size: 100px;
+  color: blueviolet;
+}
+.writing3 {
+  color: blue;
+  display: inline;
+}
+.writing4 {
+  font-weight: bold;
+  font-size: 20px;
+  display: inline;
+}
+.end {
+  margin-top: 100px;
 }
 </style>
