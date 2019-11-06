5a834cd6fcbef1f0e8a30d0c1d5e00b11c45c186
yfq512 <1085561337@qq.com>
2019-11-06 09:13:30 +0000

    修改数据真正标签获取方式，修改Bug

--- howtoMala/UseMala_example.py
+++ howtoMala/UseMala_example.py
@@ -9,8 +9,12 @@ from keras.utils.vis_utils import plot_model
 
 NUM_EPOCHS = 20
 INIT_LR = 1e-1
-BS = 100
-
+BS = 0 #BS值必须为被预测图片的数量
+predict_root = "./testing_cross_domain"
+file_temp = os.listdir(predict_root)
+for i in file_temp:
+    BS = BS + len(os.listdir(os.path.join(predict_root,i)))
+    
 #totalTest_cross_domain = len(list(paths.list_images(config.TEST_PATH_CROSS_DOMAIN)))
 totalTest_cross_domain=2
 print("  totalTest_cross_domain="+str(totalTest_cross_domain))
@@ -18,13 +22,12 @@ print("  totalTest_cross_domain="+str(totalTest_cross_domain))
 # initialize the testing generator for cross domain
 valAug = ImageDataGenerator(rescale=1 / 255.0)
 testGen_cross_domain = valAug.flow_from_directory(
-        "./testing_cross_domain",
+        predict_root,
 	class_mode="categorical",
 	target_size=(64, 64),
 	color_mode="rgb",
 	shuffle=False,
 	batch_size=BS)
-
 model=load_model("mala.h5")
 # 保存模型结构图
 plot_model(model, to_file='model1.png',show_shapes=True)
@@ -37,13 +40,20 @@ predIdxs = model.predict_generator(testGen_cross_domain,
 
 classes = list(np.argmax(predIdxs, axis=1))
 filenames = testGen_cross_domain.filenames
-for f in zip(filenames, testGen_cross_domain.classes, classes):
-   print (f)
+# 根据细胞所在文件夹定义真正标签
+labels_trues = []
+for file in filenames:
+    if file.split('/')[0][-1] == 'P':
+        labels_trues.append(0)
+    elif file.split('/')[0][-1] == 'N':
+        labels_trues.append(1)
+for filename,labels_true,classe in zip(filenames, labels_trues, classes):
+   print ('图片:{0}-真正标签:{1}-预测标签:{2}'.format(filename,labels_true,classe))
 
 # for each image in the testing set we need to find the index of the
 # label with corresponding largest predicted probability
 predIdxs = np.argmax(predIdxs, axis=1)
 
 # show a nicely formatted classification report
-print('\n',classification_report(testGen_cross_domain.classes, predIdxs,
+print('\n',classification_report(labels_trues, predIdxs,
 	target_names=testGen_cross_domain.class_indices.keys()))
