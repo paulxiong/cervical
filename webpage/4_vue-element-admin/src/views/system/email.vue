62c9df7c13abcebbf99b84ba0bb5d3882b3c4478
qixia717138552 <717138552@qq.com>
2019-11-19 17:06:47 +0800

    邮箱设置页面细节修改

--- webpage/4_vue-element-admin/src/views/system/email.vue
+++ webpage/4_vue-element-admin/src/views/system/email.vue
@@ -22,11 +22,14 @@ export default {
       `
         <h1 style="text-align: left;">讯动医疗邮箱注册验证码</h1>
         <br/>
-        <p style="text-align: left;">您好，您正在使用<span style="color: blue;">****xundong@gmail.com</span>注册账号</p>
-        <p style="text-align: left;">您的注册验证码为：000000</p>
+        <p style="text-align: left;">尊敬的用户:</p>
+        <p style="text-align: left;">&nbsp;&nbsp;&nbsp;&nbsp;您好，您正在使用<span style="color: blue;"><sub>****</sub>xundong@gmail.com</span>注册账号</p>
+        <p style="text-align: left;">&nbsp;&nbsp;&nbsp;&nbsp;您的注册验证码为：<span style="color: red; font-weight: bold;">000000</span><span style="color: green;">&nbsp;(10分钟内有效)</span></p>
+        <p style="text-align: left;">&nbsp;&nbsp;&nbsp;&nbsp;感谢您本次的使用</p>
         <br/>
         <hr/>
         <p style="text-align: right;">谢谢！</p>
+        <p style="text-align: right;">讯动医疗团队</p>
       `
     }
   }
