a3329a5be88ae8ebaa6a505e8e324fc1af474442
qixia717138552 <717138552@qq.com>
2019-11-21 14:45:48 +0800

    修改个人信息详情页面

--- webpage/4_vue-element-admin/src/views/user/userInfo.vue
+++ webpage/4_vue-element-admin/src/views/user/userInfo.vue
@@ -1,30 +1,46 @@
 <template>
   <div class="text-center">
-    <img v-if="newAvatar" :src="newAvatar+'?width=160'">
-    <img v-else :src="avatar+'?width=160'">
-    <button id="pick-avatar">上传一张图片作为头像</button>
+    <div class="imgreplace">
+      <img v-if="newAvatar" :src="newAvatar+'?width=160'">
+      <img v-else :src="avatar+'?width=160'">
+      <el-button id="pick-avatar" type="info" class="upload" round>上传头像</el-button>
+    </div>
     <avatar-cropper
       trigger="#pick-avatar"
       :upload-headers="headers"
       :upload-url="uploadURL"
       @uploaded="handleUploaded"
     />
-
-    <el-form ref="ruleForm" :model="ruleForm" :rules="rules" label-width="100px" class="demo-ruleForm">
-      <el-form-item label="昵称" prop="name">
-        <el-input v-model="ruleForm.name" />
-      </el-form-item>
-      <el-form-item label="手机" prop="mobile">
-        <el-input v-model="ruleForm.mobile" />
-      </el-form-item>
-      <el-form-item label="介绍" prop="introduction">
-        <el-input v-model="ruleForm.introduction" type="textarea" />
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
-        <el-button @click="resetForm('ruleForm')">重置</el-button>
-      </el-form-item>
-    </el-form>
+    <div class="info">
+      <el-form ref="ruleForm" :model="ruleForm" :rules="rules" label-width="100px" class="demo-ruleForm">
+        <el-form-item label="昵称" prop="name">
+          <el-input v-model="ruleForm.name" />
+        </el-form-item>
+        <el-form-item label="邮箱账号" prop="email">
+          <el-input v-model="ruleForm.email" :disabled="true" />
+        </el-form-item>
+        <el-form-item label="性别" prop="sex">
+          <el-select v-model="ruleForm.sex" placeholder="请选择您的性别" class="sex">
+            <el-option label="男" value="man" />
+            <el-option label="女" value="women" />
+            <el-option label="保密" value="secret" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="手机" prop="mobile">
+          <el-input v-model="ruleForm.mobile" />
+        </el-form-item>
+        <el-form-item label="创建时间" prop="created_at">
+          <el-input v-model="ruleForm.created_at" :disabled="true" />
+        </el-form-item>
+        <el-form-item label="介绍" prop="introduction">
+          <el-input v-model="ruleForm.introduction" type="textarea" :autosize="{ minRows: 10, maxRows: 20}" />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm')">保存</el-button>
+          <el-button @click="resetForm('ruleForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -34,6 +50,7 @@ import { APIUrl } from '@/const/config'
 import { getToken } from '@/utils/auth'
 import { mapGetters } from 'vuex'
 import { updateUserInfo } from '@/api/user'
+// import { parseTime } from '@/utils/index'
 
 export default {
   components: { AvatarCropper },
@@ -79,6 +96,8 @@ export default {
     this.ruleForm.mobile = this.mobile
     this.ruleForm.introduction = this.introduction
     this.newAvatar = this.avatar
+    this.ruleForm.email = this.email
+    this.ruleForm.created_at = this.created_at
   },
   methods: {
     handleUploaded(resp) {
@@ -114,3 +133,26 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.text-center {
+}
+.imgreplace {
+  float: left;
+  margin: 50px;
+}
+.upload {
+  margin-top: 200px;
+  position: absolute;
+  margin-left: -130px;
+}
+.sex {
+  float: left;
+}
+.info {
+  overflow:hidden;
+  width: 40%;
+  border-left: 1px solid #ccc;
+  margin-top: 100px;
+}
+</style>
