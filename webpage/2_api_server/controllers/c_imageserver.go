efb9cd73903c0a197f86f2a3182fbf76111e2080
lambda-zhang <ggxxde@163.com>
2019-11-29 13:44:54 +0800

    打开图片防盗链

--- webpage/2_api_server/controllers/c_imageserver.go
+++ webpage/2_api_server/controllers/c_imageserver.go
@@ -2,28 +2,27 @@ package controllers
 
 import (
 	"net/http"
-	"path"
 	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
 
 	f "github.com/paulxiong/cervical/webpage/2_api_server/functions"
+	m "github.com/paulxiong/cervical/webpage/2_api_server/models"
 )
 
 // checkReferer 防盗链功能
 func checkReferer(c *gin.Context, rootURLpath string) {
-	ref := c.Request.Referer()
-	// 非法请求
-	if ref == "" {
-		c.Request.URL.Path = path.Join("/imgs", "scratch/static/forbidden.png")
+	if m.SystemCfg == nil || m.SystemCfg.RefererEn != 2 {
 		return
 	}
-	referers := [...]string{
-		"http://medical.raidcdn.cn:3000",
-		"https://medical.raidcdn.cn:3000",
-		"http://dev.medical.raidcdn.cn:3000",
-		"https://dev.medical.raidcdn.cn:3000",
+
+	referers := m.SystemCfg.Referers2
+	ref := c.Request.Referer()
+	// 非法请求
+	if ref == "" || len(referers) < 1 {
+		c.Request.URL.Path = m.SystemCfg.Referer401URL
+		return
 	}
 	//检查referer是否合法, 合法的不做任何操作直接返回
 	for _, _ref := range referers {
@@ -31,13 +30,13 @@ func checkReferer(c *gin.Context, rootURLpath string) {
 			// 图片不存在
 			ret, err := f.PathExists(c.Request.URL.Path)
 			if ret == false || err != nil {
-				c.Request.URL.Path = path.Join("/imgs", "scratch/static/notfound.png")
+				c.Request.URL.Path = m.SystemCfg.Referer404URL
 			}
 			return
 		}
 	}
 
-	c.Request.URL.Path = path.Join("/imgs", "scratch/static/forbidden.png")
+	c.Request.URL.Path = m.SystemCfg.Referer401URL
 	return
 }
 
@@ -52,16 +51,21 @@ func checkReferer(c *gin.Context, rootURLpath string) {
 // @Success 200 {string} json "{"ping": "pong",	"status": 200}"
 // @Router /imgs [get]
 func ImageAPI(c *gin.Context) {
+	imgexpires := 1
+	if m.SystemCfg != nil && m.SystemCfg.ImgExpires > 0 {
+		imgexpires = m.SystemCfg.ImgExpires
+	}
+
 	cfg := f.ImageServerSettings{
 		MaxWidth:     4096,
 		MaxHeight:    4096,
 		ImgDir:       ".",
 		Cachedir:     "cache",
 		MemCacheSize: int64(128 * (1 << 20)), //128M
-		HTTPExpires:  1 * 24 * time.Hour,
+		HTTPExpires:  time.Duration(imgexpires) * time.Hour,
 	}
 
-	// checkReferer(c, "/imgs")
+	checkReferer(c, "/imgs")
 	h := http.StripPrefix("/imgs", f.ImageServer(&cfg))
 	h.ServeHTTP(c.Writer, c.Request)
 }
