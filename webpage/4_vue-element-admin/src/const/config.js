0739c860c5afe492645ffef88f5ca0e28a440207
lambda-zhang <ggxxde@163.com>
2019-12-16 21:06:19 +0800

    Revert "del config.js"
    This reverts commit 24c30ed1787ca4b0ec94bb8bfcc0c91db1efb2a9.

---
+++ webpage/4_vue-element-admin/src/const/config.js
@@ -0,0 +1,8 @@
+// for release
+export const APIUrl =
+  process.env.VUE_APP_API === 'production'
+    ? 'http://medical.raidcdn.cn:3000'
+    : 'http://dev.medical.raidcdn.cn:3000'
+// for local debug
+// export const APIUrl = 'http://1668u455y4.imwork.net:9000/'
+// export const APIUrl = 'http://medical.raidcdn.cn:3000'
