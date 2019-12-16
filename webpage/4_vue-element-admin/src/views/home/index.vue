18edcc92938b2ce40c2337af73a295e6e82ce5bb
qixia717138552 <717138552@qq.com>
2019-12-16 15:53:26 +0800

    首页修改

--- webpage/4_vue-element-admin/src/views/home/index.vue
+++ webpage/4_vue-element-admin/src/views/home/index.vue
@@ -3,10 +3,10 @@
     <panel-group />
 
     <div class="row-box flex">
-      <transaction-table style="width: 48%;" />
+      <medical-report style="width: 48%;" />
       <best-model style="width: 48%;" />
       <data-sets style="width: 48%; margin: 40px 0;" />
-      <medical-report style="width: 48%; margin: 40px 0;" />
+      <transaction-table style="width: 48%; margin: 40px 0;" />
     </div>
   </div>
 </template>
