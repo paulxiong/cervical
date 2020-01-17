89a9697c5d31ff4b9a689ead65c2a10b1bf93b77
lambda-zhang <ggxxde@163.com>
2020-01-17 18:12:15 +0800

    Revert "任务的请求放到一个队列里面，防止冲突"
    This reverts commit 7baea4dbcb7600926b38011027055a072651311c.

--- webpage/2_api_server/routes/routes.go
+++ webpage/2_api_server/routes/routes.go
@@ -60,18 +60,8 @@ func Router() *gin.Engine {
 
 	api1 := r.Group("/api1")
 	api1.GET("/ping", ctr.Pong) //不需要登录就能ping的API
-
 	// 任务
-	jobqueue := make(chan *gin.Context)
-	go func() {
-		for {
-			c := <-jobqueue
-			ctr.GetOneJob(c)
-		}
-	}()
-	api1.POST("/job", func(c *gin.Context) {
-		jobqueue <- c
-	})
+	api1.POST("/job", ctr.GetOneJob)
 	api1.POST("/jobresult", ctr.SetJobResult)
 
 	// 临时测试使用
