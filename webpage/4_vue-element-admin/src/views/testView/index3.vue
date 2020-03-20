094625fe5820a3d3187ebe2adc505782ce63a2e0
inpm <inpm.cy@gmail.com>
2020-03-20 17:00:43 +0800

    新增上传组件文档

--- webpage/4_vue-element-admin/src/views/testView/index3.vue
+++ webpage/4_vue-element-admin/src/views/testView/index3.vue
@@ -3,6 +3,7 @@
     <uploader-unsupport />
     <uploader-drop>
       <p>Drop files here to upload or</p>
+      <el-link href="https://github.com/simple-uploader/vue-uploader/blob/master/README_zh-CN.md">上传组件文档地址</el-link>
       <uploader-btn>select files</uploader-btn>
       <uploader-btn :attrs="attrs">select images</uploader-btn>
       <uploader-btn :directory="true">select folder</uploader-btn>
