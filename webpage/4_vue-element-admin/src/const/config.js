b86a90f7413516e4ccea8777e69afc6304a4a1db
unknown <inpm.cy@gmail.com>
2020-04-01 13:10:45 +0800

    config恢复

--- webpage/4_vue-element-admin/src/const/config.js
+++ webpage/4_vue-element-admin/src/const/config.js
@@ -1,12 +1,12 @@
 // for release
-// export const APIUrl =
-//   process.env.VUE_APP_API === 'production'
-//     ? 'http://medical.raidcdn.cn:3000'
-//     : 'http://dev.medical.raidcdn.cn:3000'
-// export const WSURL =
-//   process.env.VUE_APP_API === 'production'
-//     ? 'ws://medical.raidcdn.cn:3000'
-//     : 'ws://dev.medical.raidcdn.cn:3000'
+export const APIUrl =
+  process.env.VUE_APP_API === 'production'
+    ? 'http://medical.raidcdn.cn:3000'
+    : 'http://dev.medical.raidcdn.cn:3000'
+export const WSURL =
+  process.env.VUE_APP_API === 'production'
+    ? 'ws://medical.raidcdn.cn:3000'
+    : 'ws://dev.medical.raidcdn.cn:3000'
 // for local debug
-export const WSURL = 'http://medical.raidcdn.cn:3000'
-export const APIUrl = 'http://medical.raidcdn.cn:3000'
+// export const WSURL = 'http://medical.raidcdn.cn:3000'
+// export const APIUrl = 'http://medical.raidcdn.cn:3000'
