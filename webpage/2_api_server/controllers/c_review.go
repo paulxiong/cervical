d43c8202d3fca973aae2c0208fc667d353779128
lambda-zhang <ggxxde@163.com>
2020-04-01 16:40:27 +0800

    漏提交

--- webpage/2_api_server/controllers/c_review.go
+++ webpage/2_api_server/controllers/c_review.go
@@ -271,7 +271,7 @@ func GetReviews(c *gin.Context) {
 // @Param skip query string false "skip, default 0"
 // @Param pid query string false "pid, default 0"
 // @Param owner query string false "owner, default 0, 0 只看属于自己的, 1 查看所有用户的"
-// @Param status query string false "status, default 0, 0 未审核 1 已审核 2 移除 3 管理员确认 4 全部(只在看自己的生效)
+// @Param status query string false "status, default 0, 0 未审核 1 已审核 2 移除 3 管理员确认 4 全部(只在看自己的生效)"
 // @Success 200 {object} controllers.reviewslist
 // @Router /api1/reviewsbypid [get]
 func GetReviewsByPID(c *gin.Context) {
