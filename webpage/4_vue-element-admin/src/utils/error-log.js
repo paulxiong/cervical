96e73aa9d0c3e684501d879c3175b3336f1a2d03
lambda-zhang <ggxxde@163.com>
2020-04-03 13:30:31 +0800

    调试dev版本输出调试信息

--- webpage/4_vue-element-admin/src/utils/error-log.js
+++ webpage/4_vue-element-admin/src/utils/error-log.js
@@ -7,28 +7,27 @@ import store from '@/store'
 // errorLog:'production' | ['production', 'development']
 // const { errorLog: needErrorLog } = settings
 
-// function checkNeed() {
-//   const env = process.env.NODE_ENV
-//   if (isString(needErrorLog)) {
-//     return env === needErrorLog
-//   }
-//   if (isArray(needErrorLog)) {
-//     return needErrorLog.includes(env)
-//   }
-//   return false
-// }
-
-// if (checkNeed()) {
-Vue.config.errorHandler = function(err, vm, info, a) {
-// Don't ask me why I use Vue.nextTick, it just a hack.
-// detail see https://forum.vuejs.org/t/dispatch-in-vue-config-errorhandler-has-some-problem/23500
-  Vue.nextTick(() => {
-    store.dispatch('errorLog/addErrorLog', {
-      err,
-      vm,
-      info,
-      url: window.location.href
-    })
-  })
+function isRelease() {
+  if (typeof (process.env.VUE_APP_API) !== 'undefined' && process.env.VUE_APP_API === 'production') {
+    return true
+  }
+  if (typeof (process.env.NODE_ENV) !== 'undefined' && process.env.NODE_ENV === 'production') {
+    return true
+  }
+  return false
 }
-// }
+
+if (isRelease()) {
+  Vue.config.errorHandler = function(err, vm, info, a) {
+    // Don't ask me why I use Vue.nextTick, it just a hack.
+    // detail see https://forum.vuejs.org/t/dispatch-in-vue-config-errorhandler-has-some-problem/23500
+    Vue.nextTick(() => {
+      store.dispatch('errorLog/addErrorLog', {
+        err,
+        vm,
+        info,
+        url: window.location.href
+      })
+    })
+  }
+}
