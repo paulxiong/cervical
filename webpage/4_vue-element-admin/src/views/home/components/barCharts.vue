2feb8d202837c38a43ec1c19d3592af1719e5a66
qixia717138552 <717138552@qq.com>
2020-04-09 13:17:24 +0800

    首页数据可视化展示(统计数据图形展示)

---
+++ webpage/4_vue-element-admin/src/views/home/components/barCharts.vue
@@ -0,0 +1,154 @@
+<template>
+  <div id="echartss" style="width: 900px; height: 600px;" />
+</template>
+
+<script>
+import echarts from 'echarts'
+export default {
+  data() {
+    return {
+      echarts1_option: {
+
+        title: {
+          text: '模型信息',
+          subtext: '虚假数据'
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        color: ['rgba(31,13,230,0.95)', '#ff475d', '#49ef18', '#efeb23'],
+        legend: [
+          {
+            data: ['切割模型', '分类模型']
+          },
+          {
+            top: 20,
+            data: ['迭代次数', '切割数量']
+          }
+
+        ],
+        toolbox: {
+          show: true,
+          feature: {
+            dataView: {
+              show: true, readOnly: true,
+              optionToContent: function(opt) {
+                const axisData = opt.xAxis[0].data // 坐标数据
+                const series = opt.series // 折线图数据
+                let tdHeads = '<td  style="padding: 0 10px">时间</td>' // 表头
+                let tdBodys = '' // 数据
+                series.forEach(function(item) {
+                  // 组装表头
+                  tdHeads += `<td style="padding: 0 10px">${item.name}</td>`
+                })
+                let table = `<table border="1" style="margin-left:20px;border-collapse:collapse;font-size:14px;text-align:center"><tbody><tr>${tdHeads} </tr>`
+                for (let i = 0, l = axisData.length; i < l; i++) {
+                  for (let j = 0; j < series.length; j++) {
+                    // 组装表数据
+                    tdBodys += `<td>${series[j].data[i]}</td>`
+                  }
+                  table += `<tr><td style="padding: 0 10px">${axisData[i]}</td>${tdBodys}</tr>`
+                  tdBodys = ''
+                }
+                table += '</tbody></table>'
+                return table
+              }
+            },
+            magicType: { show: true, type: ['line', 'bar'] },
+            restore: { show: true },
+            saveAsImage: { show: true }
+          }
+        },
+        calculable: true,
+        xAxis: [
+          {
+            type: 'category',
+            data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        series: [
+          {
+            name: '切割模型',
+            type: 'bar',
+            stack: '个人信息',
+            data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3],
+            markPoint: {
+              data: [
+                { type: 'max', name: '最大值' },
+                { type: 'min', name: '最小值' }
+              ]
+            },
+            markLine: {
+              data: [
+                { type: 'average', name: '平均值' }
+              ]
+            }
+          },
+          {
+            name: '分类模型',
+            type: 'bar',
+            stack: '个人信息',
+            data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3],
+            markPoint: {
+              data: [
+                { name: '最高', value: 182.2, xAxis: 7, yAxis: 183 },
+                { name: '最低', value: 2.3, xAxis: 11, yAxis: 3 }
+              ]
+            },
+            markLine: {
+              data: [
+                { type: 'average', name: '平均值' }
+              ]
+            }
+          },
+          {
+            name: '迭代次数',
+            type: 'bar',
+            stack: '细胞信息',
+            data: [2.0, 6.0, 7.0, 20.4, 21.7, 60.7, 135.6, 152.2, 56.7, 15.8, 7.0, 2.3],
+            markPoint: {
+              data: [
+                { name: '最高', value: 152.2, xAxis: 7, yAxis: 153 },
+                { name: '最低', value: 2.0, xAxis: 1, yAxis: 2 }
+              ]
+            },
+            markLine: {
+              data: [
+                { type: 'average', name: '平均值' }
+              ]
+            }
+          },
+          {
+            name: '切割数量',
+            type: 'bar',
+            stack: '细胞信息',
+            data: [1.0, 6.9, 9.0, 36.4, 48.7, 90.7, 100.6, 122.2, 40.7, 8.8, 6.0, 2.3],
+            markPoint: {
+              data: [
+                { name: '最高', value: 122.2, xAxis: 7, yAxis: 123 },
+                { name: '最低', value: 1.0, xAxis: 1, yAxis: 1 }
+              ]
+            },
+            markLine: {
+              data: [
+                { type: 'average', name: '平均值' }
+              ]
+            }
+          }
+        ]
+      }
+    }
+  },
+  mounted: function() {
+    // 基于准备好的dom，初始化echarts实例
+    const myChart = echarts.init(document.getElementById('echartss'))
+    // 绘制图表，this.echarts1_option是数据
+    myChart.setOption(this.echarts1_option)
+  }
+}
+</script>
---
+++ webpage/4_vue-element-admin/src/views/home/components/pieCharts.vue
@@ -0,0 +1,134 @@
+<template>
+  <div>
+    <div id="main" style="width: 900px;height: 600px;" />
+  </div>
+</template>
+
+<script>
+import echarts from 'echarts'
+export default {
+  name: 'Index',
+  data() {
+    return {}
+  },
+  mounted: function() {
+    this.$nextTick(function() {
+      this.getPie()
+    })
+  },
+  methods: {
+    getPie() {
+      // 绘制图表
+      var myChart = echarts.init(document.getElementById('main'))
+      // 指定图表的配置项和数据
+      var option = {
+        title: {
+          text: '细胞分布情况', // 主标题文本
+          subtext: '虚构数据', // 副标题文本
+          left: 'center'// 左对齐
+        },
+        tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b} : {c} ({d}%)'
+        },
+        legend: {// 图例组件，图例组件展现了不同系列的标记(symbol)，颜色和名字。可以通过点击图例控制哪些系列不显示
+          orient: 'vertical',
+          // top: 'middle',
+          bottom: 100,
+          left: 0,
+          textStyle: {// 图例中文字的样式
+            color: '#000',
+            fontSize: 12
+          },
+          data: [
+            'Norm正常细胞',
+            'LSIL鳞状上皮细胞低度病变',
+            'HSIL鳞状上皮细胞高度病变',
+            'HPV感染',
+            'SCC鳞状上皮细胞癌',
+            'ASCUS不典型鳞状细胞低',
+            'ASCH不典型鳞状细胞高',
+            'AGC不典型腺细胞',
+            'AIS颈管原位腺癌',
+            'ADC腺癌',
+            'T滴虫',
+            'M霉菌',
+            'HSV疱疹',
+            'X1线索细胞',
+            '阴性细胞',
+            '阳性细胞',
+            '未知类型',
+            '不是细胞'
+          ] // 图例上显示的饼图各模块上的名字
+        },
+        // 饼图中各模块的颜色
+        color: [
+          '#008B00',
+          '#7CFC00',
+          '#C0FF3E',
+          '#9370DB',
+          '#00FF7F',
+          '#BF3EFF',
+          '#0000FF',
+          '#9B30FF',
+          '#BDB76B',
+          '#BCEE68',
+          '#BCD2EE',
+          '#BC8F8F',
+          '#121212',
+          '#D8BFD8',
+          '#BA55D3',
+          '#EEC900',
+          '#EE3B3B',
+          '#B8860B'
+        ],
+        series: [
+          {
+            name: '细胞占比',
+            type: 'pie',
+            radius: '60%',
+            border: 100,
+            right: 100,
+            textStyle: {
+              fontSize: 14
+            },
+            data: [
+              { name: 'Norm正常细胞', value: 10 },
+              { name: 'LSIL鳞状上皮细胞低度病变', value: 5 },
+              { name: 'HSIL鳞状上皮细胞高度病变', value: 5 },
+              { name: 'HPV感染', value: 5 },
+              { name: 'SCC鳞状上皮细胞癌', value: 5 },
+              { name: 'ASCUS不典型鳞状细胞低', value: 5 },
+              { name: 'ASCH不典型鳞状细胞高', value: 10 },
+              { name: 'AGC不典型腺细胞', value: 5 },
+              { name: 'AIS颈管原位腺癌', value: 5 },
+              { name: 'ADC腺癌', value: 10 },
+              { name: 'T滴虫', value: 5 },
+              { name: 'M霉菌', value: 5 },
+              { name: 'HSV疱疹', value: 10 },
+              { name: 'X1线索细胞', value: 5 },
+              { name: '阴性细胞', value: 5 },
+              { name: '阳性细胞', value: 5 },
+              { name: '未知类型', value: 10 },
+              { name: '不是细胞', value: 5 }
+            ],
+            itemStyle: {// 用于设置样式
+              emphasis: {// 饼状图高亮时的背景样式
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.8)'
+              }
+            }
+          }
+        ]
+      }
+      // 使用刚指定的配置项和数据显示图表
+      myChart.setOption(option)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
