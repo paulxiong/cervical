b7b6384b6fef2a0c1ff4d32b93e9b63ebb334d00
lambda-zhang <ggxxde@163.com>
2020-04-15 17:45:01 +0800

    项目列表最后一个显示不全的问题

--- webpage/4_vue-element-admin/src/views/review/components/doctorReport.vue
+++ webpage/4_vue-element-admin/src/views/review/components/doctorReport.vue
@@ -1,22 +1,34 @@
 <template>
-  <div class="doctorreport">
+  <div>
     <keep-alive>
       <router-view v-if="$route.meta.keepAlive" />
     </keep-alive>
     <router-view v-if="!$route.meta.keepAlive" />
-    <el-table
-      v-loading="loading"
-      element-loading-text="拼命加载中"
-      :data="projectlist"
-      style="width: 100%"
-    >
-      <el-table-column width="70" label="ID" prop="id" />
-      <el-table-column label="描述" prop="desc" />
-      <el-table-column label="数据集ID" prop="did" width="80" />
-      <el-table-column label="创建者" width="80">
-        <template slot-scope="scope">
-          <el-tooltip v-if="scope.row.username" :content="scope.row.username" placement="right">
+    <div class="projectlist">
+      <el-table
+        v-loading="loading"
+        element-loading-text="拼命加载中"
+        :data="projectlist"
+        style="width: 100%"
+      >
+        <el-table-column width="70" label="ID" prop="id" />
+        <el-table-column label="描述" prop="desc" />
+        <el-table-column label="数据集ID" prop="did" width="80" />
+        <el-table-column label="创建者" width="80">
+          <template slot-scope="scope">
+            <el-tooltip v-if="scope.row.username" :content="scope.row.username" placement="right">
+              <el-image
+                style="width:36px;height:36px;border-radius:7px;"
+                :src="scope.row.userimg"
+                lazy
+              >
+                <div slot="error" class="image-slot">
+                  <i class="el-icon-picture-outline" />
+                </div>
+              </el-image>
+            </el-tooltip>
             <el-image
+              v-else
               style="width:36px;height:36px;border-radius:7px;"
               :src="scope.row.userimg"
               lazy
@@ -25,37 +37,27 @@
                 <i class="el-icon-picture-outline" />
               </div>
             </el-image>
-          </el-tooltip>
-          <el-image
-            v-else
-            style="width:36px;height:36px;border-radius:7px;"
-            :src="scope.row.userimg"
-            lazy
-          >
-            <div slot="error" class="image-slot">
-              <i class="el-icon-picture-outline" />
-            </div>
-          </el-image>
-        </template>
-      </el-table-column>
-      <el-table-column label="创建时间" prop="created_at" width="150" />
-      <el-table-column label="操作" prop="statusTime" width="200">
-        <template slot-scope="scope">
-          <el-button type="primary" size="mini" @click="goFovmap(scope.row)">查看详情</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <div class="tools flex">
-      <el-pagination
-        class="page"
-        :current-page.sync="currentPage"
-        :page-sizes="[10, 20, 30, 50]"
-        :page-size="currentPageSize"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="total"
-        @current-change="handleCurrentChange"
-        @size-change="handleSizeChange"
-      />
+          </template>
+        </el-table-column>
+        <el-table-column label="创建时间" prop="created_at" width="150" />
+        <el-table-column label="操作" prop="statusTime" width="200">
+          <template slot-scope="scope">
+            <el-button type="primary" size="mini" @click="goFovmap(scope.row)">查看详情</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div class="tools flex">
+        <el-pagination
+          class="page"
+          :current-page.sync="currentPage"
+          :page-sizes="[10, 20, 30, 50]"
+          :page-size="currentPageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total"
+          @current-change="handleCurrentChange"
+          @size-change="handleSizeChange"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -127,31 +129,31 @@ export default {
 </script>
 
 <style>
-.doctorReport {
+.projectlist {
   overflow: auto;
   height: 100%;
   padding-bottom: 30px;
-  }
-  .tools {
-    background: #fff;
-    justify-content: space-around;
-    bottom: 0px;
-    position: fixed;
-    left: 0;
-    right: 0;
-    margin-left: auto;
-    margin-right: auto;
-  }
-  .table-expand {
-    font-size: 0;
-  }
-  .table-expand label {
-    width: 90px;
-    color: #99a9bf;
-  }
-  .table-expand .el-form-item {
-    margin-right: 0;
-    margin-bottom: 0;
-    width: calc(100% / 4);
-  }
+}
+.tools {
+  background: #fff;
+  justify-content: space-around;
+  bottom: 0px;
+  position: fixed;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+}
+.table-expand {
+  font-size: 0;
+}
+.table-expand label {
+  width: 90px;
+  color: #99a9bf;
+}
+.table-expand .el-form-item {
+  margin-right: 0;
+  margin-bottom: 0;
+  width: calc(100% / 4);
+}
 </style>
