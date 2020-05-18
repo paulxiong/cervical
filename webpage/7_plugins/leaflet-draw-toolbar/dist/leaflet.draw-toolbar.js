5f7483fc18bbf9cbc10968d08389970aabb3bca6
lambda-zhang <ggxxde@163.com>
2020-05-18 21:21:58 +0800

    图标替换

--- webpage/7_plugins/leaflet-draw-toolbar/dist/leaflet.draw-toolbar.js
+++ webpage/7_plugins/leaflet-draw-toolbar/dist/leaflet.draw-toolbar.js
@@ -319,7 +319,6 @@ L.Toolbar2.EditAction.Popup.Delete = L.Toolbar2.Action.extend({
 		map.removeLayer(this._shape);
 		map.removeLayer(this.toolbar);
 
-		console.log('firing draw:deleted');
 		map.fire(L.Draw.Event.DELETED, { layers: L.layerGroup([this._shape]) });
 	}
 });
