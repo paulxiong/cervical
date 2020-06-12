67f7d2e35b999f949af183ac9e6fffcc59201dba
lambda-zhang <ggxxde@163.com>
2020-06-12 14:49:28 +0800

    部署web到armv8

---
+++ webpage/2_api_server/3_build_docker_img_armv8.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#该脚本只测试过在Linux环境执行
+swag init
+GOOS="linux" GOARCH="arm64" go build -v -o main.exe main.go
+if [ ! -f main.exe ]; then
+    echo "not found main.exe"
+    exit 1
+fi
+
+pushd web
+    npm install
+    npm run build:alpha
+popd
+
+rm -rf release.tgz
+tar -czvmf release.tgz main.exe configs/conf.ini web/dist/ web/src/const/errCode.json
+if [ ! -f release.tgz ]; then
+    echo "not found release.tgz"
+    exit 2
+fi
+
+rm -rf main.exe
+
+gitid=$(git rev-parse --short HEAD)
+
+echo "docker build -t lambdazhang/cervical:"${gitid}" -f Dockerfile.armv8 ."
