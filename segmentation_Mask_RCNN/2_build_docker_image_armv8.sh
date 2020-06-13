5544d89a8d6abd4c1e068c0dbe73d0466d075b7c
Your Name <you@example.com>
2020-06-13 22:07:18 +0800

    armv8的切割细胞环境打包

---
+++ segmentation_Mask_RCNN/2_build_docker_image_armv8.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+timestamp=$(date "+%Y%m%d%-H%-M%-S")
+
+mv mrcnn/SDK mrcnn/SDK.bk
+cp ../SDK/ mrcnn/SDK -a
+
+mkdir -p tmp
+pushd tmp
+    if [ ! -e OpenCV-3.4.3-aarch64-dev.deb ]; then
+        wget https://github.com/lambda-zhang/OpenCV_Xavier_whl/releases/download/3.4.3/OpenCV-3.4.3-aarch64-dev.deb
+    fi
+    if [ ! -e OpenCV-3.4.3-aarch64-libs.deb ]; then
+        wget https://github.com/lambda-zhang/OpenCV_Xavier_whl/releases/download/3.4.3/OpenCV-3.4.3-aarch64-libs.deb
+    fi
+    if [ ! -e OpenCV-3.4.3-aarch64-licenses.deb ]; then
+        wget https://github.com/lambda-zhang/OpenCV_Xavier_whl/releases/download/3.4.3/OpenCV-3.4.3-aarch64-licenses.deb
+    fi
+    if [ ! -e opencv_python-3.4.3-cp36-cp36m-linux_aarch64.whl ]; then
+        wget https://github.com/lambda-zhang/OpenCV_Xavier_whl/releases/download/3.4.3/opencv_python-3.4.3-cp36-cp36m-linux_aarch64.whl
+    fi
+popd
+
+docker build -f Dockerfile.armv8 -t cervical:crop_mr_${timestamp} .
+
+rm -rf mrcnn/SDK
+mv mrcnn/SDK.bk mrcnn/SDK
