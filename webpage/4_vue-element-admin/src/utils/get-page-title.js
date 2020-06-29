25d2e533094382c1fa1b94d6d064aa20a0ec42ca
lambda-zhang <ggxxde@163.com>
2020-06-29 14:15:14 +0800

    网页标题i18n

--- webpage/4_vue-element-admin/src/utils/get-page-title.js
+++ webpage/4_vue-element-admin/src/utils/get-page-title.js
@@ -1,9 +1,10 @@
 import defaultSettings from '@/settings'
 
 export default function getPageTitle(pageTitle) {
-  const title = (window._i18n ? window._i18n.tc('web.title') : '') || defaultSettings.title
-  if (pageTitle) {
-    return `${pageTitle} - ${title}`
+  if (!window._i18n || !window._i18n.tc('web.title')) {
+    return `${title}`
   }
-  return `${title}`
+  const title = (window._i18n ? window._i18n.tc('web.title') : '') || defaultSettings.title
+  const pageTitle2 = window._i18n ? window._i18n.tc(pageTitle) : ''
+  return `${pageTitle2} - ${title}`
 }
